91c15e894bc4814394c3d266478b920729a7ad44
dk2122212 <derickjogador1@gmail.com>
2026-03-19 12:07:45 -0300

    API Contato com endpoints GET POST PUT DELETE

--- contato-api/src/main/java/com/derick/contato_api/controller/ContatoApiApplication.java
+++ contato-api/src/main/java/com/derick/contato_api/controller/ContatoApiApplication.java
@@ -1,4 +1,4 @@
-package com.derick.contato_api;
+package com.derick.contato_api.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
